a8f7a5c3fe7c73fe5b15e7bd6c16243db1ccd76b
liyen ho <liyenho@gmail.com>
2017-07-19 16:37:14 -0700

    forgot to disable switch CTRL_DYNAMIC_MOD

--- tx/atmel/samg55/fpga/nios-0/proj-tx/src/si446x/app/radio.h
+++ tx/atmel/samg55/fpga/nios-0/proj-tx/src/si446x/app/radio.h
@@ -113,7 +113,7 @@ U8 range_mode_configure(U8 range); // added for dynamic range configuration
 U8 ctrl_band_select(U8 len, U8 *ch_param); // ctrl radio band selection
 
 void cap_bank_calibrate();
-#define CTRL_DYNAMIC_MOD
+//#define CTRL_DYNAMIC_MOD
  #ifdef CTRL_DYNAMIC_MOD
   void process_range_mode(uint32_t tick_curr,uint32_t tick_prev);
  #endif
